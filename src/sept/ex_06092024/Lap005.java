0d5ba1f87a9d8980aeb98c5d2b52db7fe05a0e72
Numaan <quazinumaan@gmail.com>
2024-10-12 00:57:36 +0530

    Task1

--- src/sept/ex_06092024/Lap005.java
+++ src/sept/ex_06092024/Lap005.java
@@ -3,5 +3,7 @@ package sept.ex_06092024;
 public class Lap005 {
     public static void main(String[] args) {
         System.out.println("Push the code on git");
+        int a = 15, b= 25, c= 10, d= 20;
+        System.out.println(a + b );
     }
 }
